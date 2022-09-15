93a218fe0947ce2c97fa4219799951ec29477c7f
Bảo Huỳnh Lê Quốc <baohuynh.pix@gmail.com>
2022-09-15 18:06:56 +0700

    fix font

--- components/menu/MenuItem.jsx
+++ components/menu/MenuItem.jsx
@@ -6,11 +6,11 @@ export function MenuItem({ name, href, isActive }) {
     <Link href={href}>
       <a
         className={clsx(
-          ` text-white md:text-black text-base font-[Rokkitt] font-normal
+          ` text-white md:text-black text-base font-Rokkitt font-normal
             hover:text-primary
           `,
           {
-            "text-[#595959] md:text-primary border-b-2 md:border-primary border-[#595959]": isActive,
+            "text-[#595959] md:text-primary border-b-2 md:border-primary border-secondary": isActive,
           }
         )}
       >
