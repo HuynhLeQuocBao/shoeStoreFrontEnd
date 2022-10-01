e05d53ca189b42fa3e1ebf41dc036938cff5374c
Bảo Huỳnh Lê Quốc <baohuynh.pix@gmail.com>
2022-10-01 14:25:27 +0700

    fix nextauth do Cat bao

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,41 +1,29 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import FacebookProvider from "next-auth/providers/facebook";
-import CredentialsProvider from "next-auth/providers/credentials";
+import CredentialsProvider from 'next-auth/providers/credentials';
 import axios from "axios";
 import { authApi } from "@/apiClient/auth";
 
 export default NextAuth({
+
 	providers: [
 		CredentialsProvider({
-			name: "credentials",
-			async authorize(credentials) {
-				try {
-					console.log(
-						"🚀 ~ file: [...nextauth].js ~ line 12 ~ authorize ~ credentials",
-						credentials
-					);
-					// const result = await authApi.userLogin({
-					// 	email: credentials.email,
-					// 	password: credentials.password,
-					// });
-					const result = await axios.post(
-						`${process.env.API_URL}/api/v1/auth/credentials`,
-						{
-							email: credentials.email,
-							password: credentials.password,
-						}
-					);
-					console.log("Result", result.tokens.user);
-					if (result) return { data: result };
-					else {
-						return null;
-					}
-				} catch (error) {
-					return null;
-				}
-			},
-		}),
+      type: 'credentials',
+      async authorize(credentials) {
+        try {
+          const result = await authApi.userLogin({
+            email: credentials.email,
+            password: credentials.password,
+          });
+					console.log(result);
+
+          return result;
+        } catch (error) {
+          return null;
+        }
+      },
+    }),
 		GoogleProvider({
 			clientId: process.env.GOOGLE_CLIENT_ID,
 			clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -48,7 +36,8 @@ export default NextAuth({
 	callbacks: {
 		async jwt({ token, user, account }) {
 			try {
-				if (account.provider != null && user) {
+				// Call multiple if use useSession
+				if (user) {
 					const result = await axios.post(
 						`${process.env.API_URL}/api/v1/auth/${account.provider}`,
 						{
@@ -61,18 +50,15 @@ export default NextAuth({
 
 					console.log("Data", result.data);
 
-					token.accessToken = result.data.token.accessToken;
+					token.accessToken = result.data.token;
 					// token.expAccessToken = result.data.expires_at;
 					token.user = result.data.newUser || result.data.userUpdated;
-				} else {
-					console.log("test", data);
-					token.accessToken = data.tokens.accessToken;
-					token.user = data.tokens.user;
 				}
+
 				return token;
 			} catch (error) {
 				// console.log("error", error);
-				// console.log(error?.response?.data);
+				console.log(error?.response?.data);
 				return {
 					isError: true,
 				};
@@ -82,10 +68,11 @@ export default NextAuth({
 			if (token.isError) {
 				return null;
 			}
+
 			session.accessToken = token.accessToken;
 			session.user = token.user;
 			// session.expires = new Date(token.expAccessToken).toISOString();
-			console.log("Session", session);
+
 			return session;
 		},
 	},
