557c0875de6b29ae33c95b97cb0fed9c4b38ff9a
Cat Le <catle4551@gmail.com>
2022-11-26 03:20:02 +0700

    final

--- components/section/myOrder/ListOrders.jsx
+++ components/section/myOrder/ListOrders.jsx
@@ -3,98 +3,96 @@ import { useEffect } from "react";
 import { HiOutlineChevronRight } from "react-icons/hi";
 import { orderApi } from "@/apiClient/order";
 import { useState } from "react";
-import Moment from 'moment';
+import Moment from "moment";
 
 export function ListOrders() {
-    const [orderList, setOrderList] = useState([])
-    useEffect(() => {
-        try {
-            const fetchGetOrder = async () => {
-                const data = await orderApi.getAllOrder()
-                setOrderList(data)
-            }
-            fetchGetOrder()
-        } catch (error) {
-            console.log(error)
-        }
-    }, [])
-    const changeState = (state) => {
-        if (state === 0) {
-            return 'pending'
-        } else if (state === 1) {
-            return 'confirmed'
-        } else if (state === 2) {
-            return 'In transit'
-        } else if (state === 3) {
-            return 'complete'
-        }
-    }
-    return (
-        <div className="w-full mb-16">
-            <div className='hidden md:block w-full mt-10 mb-10'>
-                <div className='w-full bg-[#f0f0f0] py-3 font-semibold text-base rounded-md items-center justify-center uppercase grid grid-cols-12 mb-6 shadow-lg'>
-                    <div className='text-center col-span-1'>
-                        <span>STT</span>
-                    </div>
-                    <div className='text-center col-span-4'>
-                        <span>STATE</span>
-                    </div>
-                    <div className='text-center col-span-1'>
-                        <span>AMOUNT</span>
-                    </div>
-                    <div className='text-center col-span-2'>
-                        <span>TOTAL</span>
-                    </div>
-                    <div className='col-span-2  text-center'>
-                        <span>DATE</span>
-                    </div>
-                    <div className='text-center col-span-2'>
-                        <span>VIEW DETAILS</span>
-                    </div>
-                </div>
-                {
-                    orderList.length > 0 ?
-                        orderList.map((item, index) => (
-                            <Link href={`/order-detail/${item._id}`} key={index}>
-                                <div className='w-full text-sm grid grid-cols-12  border border-b-2 shadow-lg rounded-3xl hover:bg-zinc-100 duration-500 py-3 mb-2 cursor-pointer'>
-                                    <div className='text-center col-span-1 flex justify-center items-center'>
-                                        <span>{index + 1}</span>
-                                    </div>
-                                    <div className='text-center col-span-4 flex justify-center items-center'>
-                                        <div
-                                            className={`
+	const [orderList, setOrderList] = useState([]);
+	useEffect(() => {
+		try {
+			const fetchGetOrder = async () => {
+				const data = await orderApi.getAllOrder();
+				setOrderList(data);
+			};
+			fetchGetOrder();
+		} catch (error) {
+			console.log(error);
+		}
+	}, []);
+	const changeState = (state) => {
+		if (state === 0) {
+			return "pending";
+		} else if (state === 1) {
+			return "confirmed";
+		} else if (state === 2) {
+			return "In transit";
+		} else if (state === 3) {
+			return "complete";
+		}
+	};
+	return (
+		<div className="w-full mb-16">
+			<div className="hidden md:block w-full mt-10 mb-10">
+				<div className="w-full bg-[#f0f0f0] py-3 font-semibold text-base rounded-md items-center justify-center uppercase grid grid-cols-12 mb-6 shadow-lg">
+					<div className="text-center col-span-1">
+						<span>STT</span>
+					</div>
+					<div className="text-center col-span-4">
+						<span>STATE</span>
+					</div>
+					<div className="text-center col-span-2">
+						<span>TOTAL</span>
+					</div>
+					<div className="col-span-2  text-center">
+						<span>DATE</span>
+					</div>
+					<div className="text-end col-span-2">
+						<span>VIEW DETAILS</span>
+					</div>
+				</div>
+				{orderList.length > 0 ? (
+					orderList.map((item, index) => (
+						<Link href={`/order-detail/${item._id}`} key={index}>
+							<div className="w-full text-sm grid grid-cols-12  border border-b-2 shadow-lg rounded-3xl hover:bg-zinc-100 duration-500 py-3 mb-2 cursor-pointer">
+								<div className="text-center col-span-1 flex justify-center items-center">
+									<span>{index + 1}</span>
+								</div>
+								<div className="text-center col-span-4 flex justify-center items-center">
+									<div
+										className={`
                                             
-                                                ${item.status === 0 ? 'bg-orange-400' :
-                                                    item.status === 1 ? 'bg-green-400' :
-                                                        item.status === 2 ? 'bg-slate-400' :
-                                                            'bg-blue-400'
-                                                }
+                                                ${
+																									item.status === 0
+																										? "bg-orange-400"
+																										: item.status === 1
+																										? "bg-green-400"
+																										: item.status === 2
+																										? "bg-slate-400"
+																										: "bg-blue-400"
+																								}
                                                 w-fit py-1 px-4 rounded-2xl uppercase text-white font-bold
                                             `}
-                                        >
-                                            {changeState(item.status)}
-                                        </div>
-                                    </div>
-                                    <div className='text-center col-span-1 flex justify-center items-center'>
-                                        <span>{item.amount}</span>
-                                    </div>
-                                    <div className='text-center col-span-2 flex justify-center items-center'>
-                                        <span>$ {item.total}</span>
-                                    </div>
-                                    <div className='col-span-2  text-center flex justify-center items-center'>
-                                        <span>{item.createdAt}</span>
-                                    </div>
-                                    <div className='flex justify-end items-center col-span-2 pr-14'>
-                                        <HiOutlineChevronRight />
-                                    </div>
-                                </div>
-                            </Link>
-                        )) :
-                        <div className='w-full text-sm border border-b-2 shadow-lg rounded-lg text-center duration-500 py-10 mb-2'>No data</div>
-
-                }
-
-            </div>
-        </div>
-    )
+									>
+										{changeState(item.status)}
+									</div>
+								</div>
+								<div className="text-center col-span-2 flex justify-center items-center">
+									<span>$ {item.total}</span>
+								</div>
+								<div className="col-span-2  text-center flex justify-center items-center">
+									<span>{item.createdAt}</span>
+								</div>
+								<div className="flex justify-end items-center col-span-2 pr-14">
+									<HiOutlineChevronRight />
+								</div>
+							</div>
+						</Link>
+					))
+				) : (
+					<div className="w-full text-sm border border-b-2 shadow-lg rounded-lg text-center duration-500 py-10 mb-2">
+						No data
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
--- components/section/orders/Checkout.jsx
+++ components/section/orders/Checkout.jsx
@@ -1,208 +1,196 @@
 /* This example requires Tailwind CSS v2.0+ */
-import { Container } from '@/components/common'
+import { Container } from "@/components/common";
 import { useForm, Controller } from "react-hook-form";
-import { cartApi } from '@/apiClient/cartAPI';
-import { useState, useEffect } from 'react'
-import { useRouter } from 'next/router'
-import { ProgressCart } from '.';
-import { useSession } from 'next-auth/react';
-
+import { cartApi } from "@/apiClient/cartAPI";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { ProgressCart } from ".";
+import { useSession } from "next-auth/react";
 
 export function Checkout() {
-    const router = useRouter()
-    const { data: session } = useSession();
-    console.log('session', session?.user?.fullname)
-    const [data, setData] = useState([])
-    const [subTotal, setSubTotal] = useState(0)
-    const [fullname, setFullname] = useState(session?.user?.fullname)
-    const [address, setAddress] = useState(session?.user?.address)
-    const [numberphone, setNumberPhone] = useState(session?.user?.numberPhone)
-    const [email, setEmail] = useState(session?.user?.email)
-    const {
-        register,
-        control,
-        handleSubmit,
-        formState: { errors },
-        setValue,
-    } = useForm({
-        mode: "onChange"
-    });
-    useEffect(() => {
-        try {
-            const fetchCart = async () => {
-                const dataCart = await cartApi.getAllCart();
-                setData(dataCart?.results)
-                setSubTotal(dataCart?.totalCart)
-                if (dataCart?.totalCart === 0) {
-                    router.push('/shopping-cart')
-                }
-                console.log(dataCart)
-            };
-            fetchCart();
-        } catch (error) {
-            console.log("Error");
-        }
-    }, []);
-    const onSubmit = (data) => {
-        try {
-            const fetchCheckoutCart = async () => {
-                const result = await cartApi.checkoutCart({
-                    fullname: data.fullname,
-                    address: data.address,
-                    numberPhone: data.numberPhone,
-                    email: data.email,
-                });
-                console.log(result)
-                if (result) {
-                    router.push('/order-complete')
-                    setTimeout(() => {
-
-                        router.reload(window.location.pathname)
-                    }, 500);
-                }
-            };
-            fetchCheckoutCart();
-        } catch (error) {
-            console.log("Error", error);
-        }
-
-    }
-    return (
-        <Container>
-            <ProgressCart />
-            <div className=' w-full mt-32 mb-10'>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                    <div className='grid grid-cols-12 gap-4 w-full'>
-                        <div className='col-span-7 w-full bg-[#f5f5f5] p-4 shadow-lg rounded-lg'>
-                            <div className='font-bold text-2xl py-4'>
-                                <h1>Billing Details</h1>
-                            </div>
-                            <div className='w-full my-4 grid grid-cols-12 gap-3 '>
-                                <div className='col-span-12'>
-                                    <Controller
-                                        control={control}
-                                        name='fullname'
-                                        render={({ field }) => (
-                                            <div className='col-span-6'>
-                                                <label htmlFor='fullname'>FIRST NAME</label>
-                                                <input
-                                                    id='fullname'
-                                                    placeholder='Full Name'
-                                                    defaultValue={fullname}
-                                                    // onChange={e => setFullname(e.target.value)}
-                                                    className='w-full p-4 rounded-xl my-2'
-                                                    {...register("fullname")}
-
-                                                />
-                                            </div>
-                                        )}
-                                    />
-                                </div>
-                            </div>
-                            <div className='w-full my-4'>
-                                <Controller
-                                    control={control}
-                                    name='address'
-                                    render={({ field }) => (
-                                        <div>
-                                            <label htmlFor='address'>ADDRESS</label>
-                                            <input
-                                                id='address'
-                                                placeholder='Address'
-                                                defaultValue={address}
-                                                // onChange={e => setAddress(e.target.value)}
-                                                className='w-full p-4 rounded-xl my-2'
-                                                {...register("address")}
-
-                                            />
-                                        </div>
-                                    )}
-                                />
-                            </div>
-                            <div className='w-full my-4'>
-                                <Controller
-                                    control={control}
-                                    name='numberPhone'
-                                    render={({ field, value }) => (
-                                        <div>
-                                            <label htmlFor='phone'>PHONE</label>
-                                            <input
-                                                id='phone'
-                                                placeholder='Number Phone'
-                                                defaultValue={numberphone}
-                                                // onChange={e => setNumberPhone(e.target.value)}
-                                                className='w-full p-4 rounded-xl my-2'
-                                                {...register("numberPhone")}
-                                            />
-                                        </div>
-                                    )}
-                                />
-                            </div>
-                            <div className='w-full my-4'>
-                                <Controller
-                                    control={control}
-                                    name='email'
-                                    render={({ field }) => (
-                                        <div>
-                                            <label htmlFor='address'>EMAIL</label>
-                                            <input
-                                                id='email'
-                                                name='email'
-                                                defaultValue={email}
-                                                // onChange={e => setEmail(e.target.value)}
-                                                placeholder='Email'
-                                                className='w-full p-4 rounded-xl my-2'
-                                                {...register("email")}
-
-                                            />
-                                        </div>
-                                    )}
-                                />
-                            </div>
-
-                        </div>
-                        <div className='col-span-5 '>
-                            <div className='w-full mb-8 p-4 bg-[#f5f5f5] font-medium shadow-lg rounded-lg'>
-                                <div className='font-bold text-2xl py-4'>
-                                    <h1>Cart Total</h1>
-                                </div>
-                                <div className='border-b-2 my-2  w-full flex'>
-                                    <span className='w-[60%]'>Subtotal </span>
-                                    <p className='w-[40%] text-sm'>$ {subTotal}</p>
-                                </div>
-                                {
-                                    data?.map((item) => {
-                                        return (
-                                            <div key={item._id} className='border-b-2 my-2 w-full flex'>
-                                                <span className='w-[60%]'>{item?.quantity}*{item?.productName} </span>
-                                                <p className='w-[40%] text-sm'>$ {item?.total}</p>
-                                            </div>
-                                        )
-                                    })
-                                }
-                                <div className='border-b-2 my-2 w-full flex'>
-                                    <span className='w-[60%]'>Shipping </span>
-                                    <p className='w-[40%] text-sm'>$ 0</p>
-                                </div>
-                                <div className='border-b-2 my-2 w-full flex'>
-                                    <span className='w-[60%]'>Order Total: </span>
-                                    <p className='w-[40%] text-sm'>{subTotal}</p>
-
-                                </div>
-                            </div>
-                            <div className='w-full my-1 flex justify-center items-center'>
-                                <button className='w-full md:w-full py-2 rounded-2xl bg-green-400 cursor-pointer hover:bg-green-600 font-bold duration-500 hover:text-white'>
-                                    Complete
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-
-
-            </div>
-
-        </Container>
-    )
-
+	const router = useRouter();
+	const { data: session } = useSession();
+	console.log("session", session?.user?.fullname);
+	const [data, setData] = useState([]);
+	const [subTotal, setSubTotal] = useState(0);
+	const [fullname, setFullname] = useState(session?.user?.fullname);
+	const [address, setAddress] = useState(session?.user?.address);
+	const [numberphone, setNumberPhone] = useState(session?.user?.numberPhone);
+	const [email, setEmail] = useState(session?.user?.email);
+	const {
+		register,
+		control,
+		handleSubmit,
+		formState: { errors },
+		setValue,
+	} = useForm({
+		mode: "onChange",
+	});
+	useEffect(() => {
+		try {
+			const fetchCart = async () => {
+				const dataCart = await cartApi.getAllCart();
+				setData(dataCart?.results);
+				setSubTotal(dataCart?.totalCart);
+				if (dataCart?.totalCart === 0) {
+					router.push("/shopping-cart");
+				}
+				console.log(dataCart);
+			};
+			fetchCart();
+		} catch (error) {
+			console.log("Error");
+		}
+	}, []);
+	const onSubmit = (data) => {
+		try {
+			const fetchCheckoutCart = async () => {
+				const result = await cartApi.checkoutCart({
+					fullname: data.fullname,
+					address: data.address,
+					numberPhone: data.numberPhone,
+					email: data.email,
+				});
+				console.log(result);
+				if (result) {
+					router.push("/order-complete");
+					setTimeout(() => {
+						router.reload(window.location.pathname);
+					}, 500);
+				}
+			};
+			fetchCheckoutCart();
+		} catch (error) {
+			console.log("Error", error);
+		}
+	};
+	return (
+		<Container>
+			<ProgressCart />
+			<div className=" w-full mt-32 mb-10">
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<div className="grid grid-cols-12 gap-4 w-full">
+						<div className="col-span-7 w-full bg-[#f5f5f5] p-4 shadow-lg rounded-lg">
+							<div className="font-bold text-2xl py-4">
+								<h1>Billing Details</h1>
+							</div>
+							<div className="w-full my-4 grid grid-cols-12 gap-3 ">
+								<div className="col-span-12">
+									<Controller
+										control={control}
+										name="fullname"
+										render={({ field }) => (
+											<div className="col-span-6">
+												<label htmlFor="fullname">FULL NAME</label>
+												<input
+													id="fullname"
+													placeholder="Full Name"
+													defaultValue={fullname}
+													// onChange={e => setFullname(e.target.value)}
+													className="w-full p-4 rounded-xl my-2"
+													{...register("fullname")}
+												/>
+											</div>
+										)}
+									/>
+								</div>
+							</div>
+							<div className="w-full my-4">
+								<Controller
+									control={control}
+									name="address"
+									render={({ field }) => (
+										<div>
+											<label htmlFor="address">ADDRESS</label>
+											<input
+												id="address"
+												placeholder="Address"
+												defaultValue={address}
+												// onChange={e => setAddress(e.target.value)}
+												className="w-full p-4 rounded-xl my-2"
+												{...register("address")}
+											/>
+										</div>
+									)}
+								/>
+							</div>
+							<div className="w-full my-4">
+								<Controller
+									control={control}
+									name="numberPhone"
+									render={({ field, value }) => (
+										<div>
+											<label htmlFor="phone">PHONE</label>
+											<input
+												id="phone"
+												placeholder="Number Phone"
+												defaultValue={numberphone}
+												// onChange={e => setNumberPhone(e.target.value)}
+												className="w-full p-4 rounded-xl my-2"
+												{...register("numberPhone")}
+											/>
+										</div>
+									)}
+								/>
+							</div>
+							<div className="w-full my-4">
+								<Controller
+									control={control}
+									name="email"
+									render={({ field }) => (
+										<div>
+											<label htmlFor="address">EMAIL</label>
+											<input
+												id="email"
+												name="email"
+												defaultValue={email}
+												// onChange={e => setEmail(e.target.value)}
+												placeholder="Email"
+												className="w-full p-4 rounded-xl my-2"
+												{...register("email")}
+											/>
+										</div>
+									)}
+								/>
+							</div>
+						</div>
+						<div className="col-span-5 ">
+							<div className="w-full mb-8 p-4 bg-[#f5f5f5] font-medium shadow-lg rounded-lg">
+								<div className="font-bold text-2xl py-4">
+									<h1>Cart Total</h1>
+								</div>
+								<div className="border-b-2 my-2  w-full flex">
+									<span className="w-[60%]">Subtotal </span>
+									<p className="w-[40%] text-sm">$ {subTotal}</p>
+								</div>
+								{data?.map((item) => {
+									return (
+										<div key={item._id} className="border-b-2 my-2 w-full flex">
+											<span className="w-[60%]">
+												{item?.quantity}*{item?.productName}{" "}
+											</span>
+											<p className="w-[40%] text-sm">$ {item?.total}</p>
+										</div>
+									);
+								})}
+								<div className="border-b-2 my-2 w-full flex">
+									<span className="w-[60%]">Shipping </span>
+									<p className="w-[40%] text-sm">$ 0</p>
+								</div>
+								<div className="border-b-2 my-2 w-full flex">
+									<span className="w-[60%]">Order Total: </span>
+									<p className="w-[40%] text-sm">{subTotal}</p>
+								</div>
+							</div>
+							<div className="w-full my-1 flex justify-center items-center">
+								<button className="w-full md:w-full py-2 rounded-2xl bg-green-400 cursor-pointer hover:bg-green-600 font-bold duration-500 hover:text-white">
+									Complete
+								</button>
+							</div>
+						</div>
+					</div>
+				</form>
+			</div>
+		</Container>
+	);
 }
